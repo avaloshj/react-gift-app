30d5caa84ffee264f52159ab5c462f74035c1cdc
Hugo Avalos <avaloshj@gmail.com>
2021-01-17 08:29:47 -0300

    docs

---
+++ docs/precache-manifest.8edfe97461196eafc0efe3edae7ab059.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81796e3be8ff807219547636d52f66e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43da83126478f519cad1",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43da83126478f519cad1",
+    "url": "/static/js/main.33328ce4.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,5 @@
 
 
-
 export const getGifs = async( category ) => {
 
     const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=10&api_key=90fApOR2Sb7T7paq9rcl4QTQweR8GBxd`;
